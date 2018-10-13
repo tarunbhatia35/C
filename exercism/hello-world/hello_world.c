8074b8ff5c2e4e0b5ec0d760528e5f1c2ac8ea4b
tarunbhatia35 <43297968+tarunbhatia35@users.noreply.github.com>
2018-10-13 09:54:37 +0530

    Update hello_world.c

--- exercism/hello-world/hello_world.c
+++ exercism/hello-world/hello_world.c
@@ -1,7 +1,7 @@
 #include "hello_world.h"
 #include <stdlib.h>
 #include <string.h>
-
+const char *hello(void
 const char *hello(void)
 {
     char * ans = malloc(sizeof(char) * strlen("Hello, World!"));
